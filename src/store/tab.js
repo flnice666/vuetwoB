0cf521e8b2f0ee1c2b110e1c8f721c925a9f4af4
flnice666 <flnice666@163.com>
2023-06-17 20:41:04 +0800

    测试git分支

--- src/store/tab.js
+++ src/store/tab.js
@@ -5,7 +5,7 @@ export default {
             {
                 path: '/',
                 name: 'home',
-                label: '首页',
+                label: '首页呢',
                 icon: 's-home',
                 url: 'Home/Home'
             },
